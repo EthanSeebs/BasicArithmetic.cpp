02eee313a8dd637be5a100182e84ed8df40ed504
EthanSeebs <es1003560@otc.edu>
2025-02-20 22:26:34 -0600

    Add project files.

---
+++ BasicArithmetic.cpp/BasicArithmetic.cpp.cpp
@@ -0,0 +1,20 @@
+// BasicArithmetic.cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare and initialize two integer variables
+    int num1 = 10;
+    int num2 = 3;
+
+    // Perform and output the results of each operation
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+    cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+    cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+    cout << num1 << " % " << num2 << " = " << num1 % num2 << endl;
+
+    return 0;
+}
